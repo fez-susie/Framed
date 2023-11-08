b1bc76961c03efe9e69356ee553c03432c6e064f
fez-susie <aid.desousa@stjohns.sa.edu.au>
2023-11-08 15:09:05 +1030

    Finished Site

--- breakdown.js
+++ breakdown.js
@@ -3,39 +3,27 @@ function bijou(){
     document.getElementById('standardTab').style.backgroundColor='var(--pineGreen)';
     document.getElementById('substantialTab').style.backgroundColor='var(--pineGreen)';
 
-    document.getElementById('bijouTab').style.order='2';
-    document.getElementById('standardTab').style.order='3';
-    document.getElementById('substantialTab').style.order='4';
-
-    document.getElementById('bijouDisp').style.display='grid';
-    document.getElementById('standardDisp').style.display='none';
-    document.getElementById('substantialDisp').style.display='none';
+    document.getElementById('bijou').style.display='grid';
+    document.getElementById('standard').style.display='none';
+    document.getElementById('substantial').style.display='none';
 }
 
 function standard(){
     document.getElementById('bijouTab').style.backgroundColor='var(--pineGreen)';
     document.getElementById('standardTab').style.backgroundColor='var(--red)';
     document.getElementById('substantialTab').style.backgroundColor='var(--pineGreen)';
-
-    document.getElementById('bijouTab').style.order='4';
-    document.getElementById('standardTab').style.order='2';
-    document.getElementById('substantialTab').style.order='3';
     
-    document.getElementById('bijouDisp').style.display='none';
-    document.getElementById('standardDisp').style.display='grid';
-    document.getElementById('substantialDisp').style.display='none';
+    document.getElementById('bijou').style.display='none';
+    document.getElementById('standard').style.display='grid';
+    document.getElementById('substantial').style.display='none';
 }
 
 function substantial(){
     document.getElementById('bijouTab').style.backgroundColor='var(--pineGreen)';
     document.getElementById('standardTab').style.backgroundColor='var(--pineGreen)';
     document.getElementById('substantialTab').style.backgroundColor='var(--red)';
-
-    document.getElementById('bijouTab').style.order='4';
-    document.getElementById('standardTab').style.order='3';
-    document.getElementById('substantialTab').style.order='2';
-
-    document.getElementById('bijouDisp').style.display='none';
-    document.getElementById('standardDisp').style.display='none';
-    document.getElementById('substantialDisp').style.display='grid';
+    
+    document.getElementById('bijou').style.display='none';
+    document.getElementById('standard').style.display='none';
+    document.getElementById('substantial').style.display='grid';
 }
